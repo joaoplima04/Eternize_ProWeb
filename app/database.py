cb71a41140d4ac2c4595c72c7702018e130efdb4
João Lucas <joao.plima@sempreceub.com>
2025-05-06 20:20:34 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Sucesso15!@localhost/postgres"
+DATABASE_URL = "postgresql://postgres:xxxxx@localhost/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
